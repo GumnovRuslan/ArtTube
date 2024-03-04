70f0e3ff92821b81065fa40316adfdc60b09b4c3
Gumnov Ruslan <ruslang.crocode@gmail.com>
2024-03-04 10:23:10 +0300

    fix event detail

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ map();
 // function isTrue(el, func) {
 //   if (!!el) func(el);
 // }
+
 // const filter = document.getElementById("filter");
 // const filterBtnComplete = filter.querySelector(".filter__btn-complete");
 // const filterBtnReset = filter.querySelector(".filter__btn-reset");
@@ -16,33 +17,3 @@ let checkbox = document.getElementById("checkbox-menu");
 checkbox.addEventListener("input", (e) => {
   if (window.innerWidth < 768) document.body.style.position = e.target.checked ? "fixed" : "static";
 });
-
-// const sectionEvent = document.querySelector(".event-full");
-
-// isTrue(sectionEvent, btnMore);
-
-// function btnMore(el) {
-//   const container = el.querySelector(".event-full__description-inner");
-//   const containerHeight = container.offsetHeight;
-//   const btnContainer = el.querySelector(".event-full__description-shadow");
-//   const btn = el.querySelector(".event-full__description-btn");
-//   if (window.innerWidth <= 480) {
-//     container.style.height = "300px";
-
-//     btn.addEventListener("click", (e) => {
-//       if (container.style.height == "300px") {
-//         container.style.height = `${containerHeight}px`;
-//         btn.querySelector(".event-full__description-btn-text").textContent = "Скрыть";
-//         btnContainer.style.boxShadow = "none";
-//         btnContainer.style.bottom = "0";
-//       } else {
-//         container.style.height = "300px";
-//         btn.querySelector(".event-full__description-btn-text").textContent = "Читать все";
-//         btnContainer.style.boxShadow = "0 0px 30px 20px #ffffff";
-//         btnContainer.style.bottom = "10px";
-//       }
-//     });
-//   }
-// }
-
-
